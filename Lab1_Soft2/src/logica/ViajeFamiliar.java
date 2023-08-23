36ac28da48531d4319c4e95115a259ea3df629cd
JChilito <99048313+JChilito@users.noreply.github.com>
2023-08-23 07:51:47 -0500

    Add files via upload
    Laboratorio N°1 - Herencia y Polimorfismo

---
+++ Lab1_Soft2/src/logica/ViajeFamiliar.java
@@ -0,0 +1,36 @@
+
+package logica;
+
+import java.util.Date;
+
+public class ViajeFamiliar extends Viaje {
+    
+    /**
+    * Cantidad de integrantes de la familia
+    */
+    private int familia;
+
+    public ViajeFamiliar(String origen, String destino, int costo, Date fechaSalida, Date fechaLlegada,int familia) {
+        super(origen, destino, costo, fechaSalida, fechaLlegada);
+        this.familia = familia;
+    }
+    
+    //Constructor getters and setters
+    @Override
+    public String descripcion() {
+        return "Viaje para disfrutar con toda tu familia&quot";
+    }
+    @Override
+    public String cualquierMetodo2() {
+        return "Método implementado en la clase hija viaje familiar";
+    }
+
+    public int getFamilia() {
+        return familia;
+    }
+
+    public void setFamilia(int familia) {
+        this.familia = familia;
+    }
+    
+}
